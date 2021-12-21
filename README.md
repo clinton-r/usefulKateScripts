508f4b8bc28387c2d55ec402f88d514b4693b81b
Clinton Reddekop <clinton.reddekop@gmail.com>
2021-12-21 16:11:39 -0700

    Initial commit, with clipboard appends

---
+++ README.md
@@ -0,0 +1,15 @@
+# Useful Kate Scripts
+
+A repository of scripts to extend the [Kate editor](https://kate-editor.org/).
+
+### Installation
+
+Linux: copy the script files to ~/.local/share/katepart5/script/commands/ and then restart Kate.  (This works for me on Linux Mint 20.2 with XFCE.)
+
+Windows: copy the script files to %USERPROFILE%\AppData\Local\katepart5\commands\ and then restart Kate.  (I haven't tried this.)
+
+More info [here](https://docs.kde.org/stable5/en/kate/katepart/dev-scripting.html)
+
+## clipboard_append.js
+
+Adds commands to cut&append to clipboard, copy&append to clipboard, and swap the selection with the clipboard.
---
+++ clipboard_append.js
@@ -0,0 +1,110 @@
+var katescript = {
+    "author":       "Clinton Reddekop",
+    "license":      "BSD",
+    "revision":     1,
+    "kate-version": 5.68,
+    "functions":    ["copyAppend", "cutAppend", "swapCutPaste"],
+    "actions": [
+        {   "function":     "copyAppend",
+            "name":         "Copy and Append Selection to Clipboard",
+            "category":     "Editing",
+            "shortcut":     "Ctrl+Shift+c",
+            "interactive":  "false"
+        },
+        {   "function":     "cutAppend",
+            "name":         "Cut and Append Selection to Clipboard",
+            "category":     "Editing",
+            "shortcut":     "Ctrl+Shift+x",
+            "interactive":  "false"
+        },
+        {   "function":     "swapCutPaste",
+            "name":         "Swap Selection and Clipboard Contents",
+            "category":     "Editing",
+            "shortcut":     "Ctrl+Shift+v",
+            "interactive":  "false"
+        }
+    ]
+};
+
+//  MIT License
+//  
+//  Copyright (c) 2021  Clinton J.S. Reddekop
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+// Reference: https://docs.kde.org/stable5/en/kate/katepart/dev-scripting.html
+
+require("cursor.js");
+
+function help(cmd)
+{
+    if (cmd == "copyAppend")
+    {
+        return i18n("Append a copy of the selected text to the contents of the clipboard.");
+    }
+    else if (cmd == "cutAppend")
+    {
+        return i18n("Cut the selected text and append it to the contents of the clipboard.");
+    }
+    else if (cmd == "swapCutPaste")
+    {
+        return i18n("Swap the current selection with the contents of the clipboard.");
+    }
+}
+
+function cutCopyAppend(cut)
+{
+    if (!view.hasSelection())
+    {
+        return;
+    }
+    
+    var selStr = view.selectedText();
+    var clipStr = editor.clipboardText();
+    var newClipStr = clipStr + selStr;
+    editor.setClipboardText(newClipStr);
+    if (cut)
+    {
+        view.removeSelectedText();
+    }
+}
+
+function copyAppend()
+{
+    cutCopyAppend(false);
+}
+
+function cutAppend()
+{
+    cutCopyAppend(true);
+}
+
+function swapCutPaste()
+{
+    if (!view.hasSelection())
+    {
+        return;
+    }
+    
+    var selStr = view.selectedText();
+    var clipStr = editor.clipboardText();
+    view.removeSelectedText();
+    document.insertText(view.cursorPosition(), clipStr);
+    editor.setClipboardText(selStr);
+}
